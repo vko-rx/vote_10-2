4ebc0fef62e3a178154d5168ff5128a668463564
vko-rx <amhdksalsgnl@gmail.com>
2023-10-05 20:08:16 +0900

    Main, Result 레이아웃 작성

---
+++ app/src/main/java/kr/hs/emirim/syan/vote_10_2/ResultActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.syan.vote_10_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
